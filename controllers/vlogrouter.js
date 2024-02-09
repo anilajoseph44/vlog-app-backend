d8ab48f1c327a2ae59d857ccbc2c07d1106d8834
anilajoseph44 <anilasandrajoseph@gmail.com>
2024-02-09 11:31:43 +0530

    done

--- controllers/vlogrouter.js
+++ controllers/vlogrouter.js
@@ -2,15 +2,31 @@ const express=require("express")
 const usermodel=require("../models/usermodel")
 
 const router=express.Router()
+const bcrypt=require("bcryptjs")
+
+hashpasswordgenerator=async(pwd)=>{
+    const salt=await bcrypt.genSalt(10)    //it s salt value.10 is the standard value for encryption
+    return bcrypt.hash(pwd,salt)       //for encrypting pswd,passing out passwoird and salt value
+
+}
 
 router.post("/signup",async(req,res)=>{
 
-    let data=req.body
-    let user=new usermodel(data)
-    let response=await user.save()
-    res.json(
-        {
-            status:"success"
+    let {data}={"data":req.body}      //getting request body data individually
+    let password=data.password
+    hashpasswordgenerator(password).then(
+        (hashedpassword)=>{
+            console.log(hashedpassword)
+            data.password=hashedpassword
+            console.log(data)
+            let user=new usermodel(data)
+            let response=user.save()
+            res.json(
+                {
+                    status:"success"
+                }
+            )
+
         }
     )
 
